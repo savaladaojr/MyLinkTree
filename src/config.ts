51975fcbb26e5d438ca8e24cddb64f41098c3165
Sergio Valadão Junior <savaladaojr@gmail.com>
2026-06-26 18:04:14 -0300

    Modify siteConfig profile details
    Updated profile name and bio to include MySQL.

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import type { SiteConfig } from "./types";
 
 const siteConfig: SiteConfig = {
   profile: {
-    name: "Sergio de Aguiar Valadão Junior",
+    name: "Sergio Valadão Junior",
     handle: "svcode",
-    bio: "Software Engineer | System Analyst | Full stack Developer | Backend Developer | .NET | C# | ASP.NET Core | Blazor | ReactJs | Node.js | JavaScript | TypeScript | SQL Server | PostgreSQL",
+    bio: "Software Engineer | System Analyst | Full stack Developer | Backend Developer | .NET | C# | ASP.NET Core | Blazor | ReactJs | Node.js | JavaScript | TypeScript | SQL Server | PostgreSQL | MySQL",
     avatarUrl:
       "https://media.licdn.com/dms/image/v2/C4E03AQHUgrM_Qxj4rw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1531326719203?e=1784160000&v=beta&t=FgJh6xKdvFzrWOJV7oqK-rtd3LOTsdZv393JxXS2so8",
   },
